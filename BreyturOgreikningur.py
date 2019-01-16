7d54f6eef60127e7dd9236f17650d582dc235b98
solvigb <42898098+solvigb@users.noreply.github.com>
2019-01-16 10:22:04 +0000

    Add files via upload

---
+++ BreyturOgreikningur.py
@@ -0,0 +1,34 @@
+#Sölvi Geir Björnsson
+#Breytur og Reikningur
+#11.1.2019 
+
+
+#Liður A
+
+karl = "Palli"
+kona = "Stína"
+kronur = 12
+skonumer = 45
+print(karl,"keypti skónúmer",skonumer,"fyrir",kona,"sem kostuðu",kronur,"krónur")
+
+skonumer2 = 47
+
+print("En þá mundi han að hún notar skónúmer",skonumer2)
+
+tala1 = input("sláðu inn tölu 1")
+
+tala2 = input("sláðu inn tölu 2")
+
+tala3 = input("sláðu inn tölu 3")
+
+summa1 = sum= float(tala1) + float(tala2) + float(tala3)
+
+summa2 = sum= float(tala1) * float(tala2) * float(tala3)
+
+summa3 = sum= float(tala1) - float(tala2) - float(tala3)
+
+print("Summa talnanna er:",summa1)
+print("margfeldi talnanna er:",summa2)
+print("Frádráttur talnanna er:",summa3)
+
+
---
+++ Æfingarverkefni1b.py
@@ -0,0 +1,34 @@
+#Sölvi Geir Björnsson
+#Breytur og Reikningur
+#11.1.2019 
+
+
+#Liður A
+
+karl = "Palli"
+kona = "Stína"
+kronur = 12
+skonumer = 45
+print(karl,"keypti skónúmer",skonumer,"fyrir",kona,"sem kostuðu",kronur,"krónur")
+
+skonumer = skonumer + 2
+
+print("En þá mundi han að hún notar skónúmer",skonumer)
+
+tala1 = int(input("sláðu inn tölu 1"))
+
+tala2 = int(input("sláðu inn tölu 2"))
+
+tala3 = int(input("sláðu inn tölu 3"))
+
+plus = tala1 + tala2 + tala3
+
+margfeldi = tala1 * tala2 * tala3
+
+minus = tala1 - tala2 - tala3
+
+print("Summa talnanna er:",plus)
+print("margfeldi talnanna er:",margfeldi)
+print("Frádráttur talnanna er:",minus)
+
+
